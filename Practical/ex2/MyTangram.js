61b6d45b842d96de7f8097c9724000ab8091caf0
up201706978 <up201706978@fe.up.pt>
2020-03-05 18:32:20 +0000

    trangram done and submit ready

--- Practical/ex2/MyTangram.js
+++ Practical/ex2/MyTangram.js
@@ -42,14 +42,14 @@ class MyTangram extends CGFobject {
 
         this.scene.pushMatrix();
         this.scene.translate(1,-1,0);
-        this.scene.rotate(180*Math.PI/180,0,0,1);
+        this.scene.rotate(Math.PI,0,0,1);
         this.triangle.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
         this.scene.translate(-0.5, 1.5, 0);
         this.scene.scale(0.5, 0.5, 0.5);
-        this.scene.rotate(180*Math.PI/180,0,0,1);
+        this.scene.rotate(Math.PI,0,0,1);
         this.triangle.display();
         this.scene.popMatrix();
 
