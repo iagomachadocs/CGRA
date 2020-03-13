ffba192c430bbfeea8d522118ee6ed3f8339c8ca
up201706978 <up201706978@fe.up.pt>
2020-03-13 12:44:32 +0000

    added to ex3

---
+++ Practical/ex3/MyTangram.js
@@ -0,0 +1,64 @@
+/**
+ * MyTriangle
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyTangram extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		
+		this.diamond = new MyDiamond(scene);
+        this.parallelogram = new MyParallelogram(scene);
+        this.triangleBig = new MyTriangleBig(scene);
+        this.triangle = new MyTriangle(scene);
+	}
+	display() {
+		this.scene.pushMatrix();
+        var trans = [1.0, 0.0, 0.0, 0.0,
+                     0.0, 1.0, 0.0, 0.0,
+                     0.0, 0.0, 1.0, 0.0,
+                    -1.0, 0.0, 0.0, 1.0];
+        this.scene.multMatrix(trans);
+        
+        this.diamond.display();
+        this.scene.popMatrix();
+        
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI, 1, 0, 0);
+        this.scene.translate(-1, -1, 0);
+        this.parallelogram.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0,-2,0);
+        this.scene.rotate(Math.PI,0,0,1);
+        this.triangleBig.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0,-2,0);
+        this.triangleBig.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(1,-1,0);
+        this.scene.rotate(Math.PI,0,0,1);
+        this.triangle.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(-0.5, 1.5, 0);
+        this.scene.scale(0.5, 0.5, 0.5);
+        this.scene.rotate(Math.PI,0,0,1);
+        this.triangle.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(-2, -1, 0);
+        this.scene.scale(0.75, 0.75, 0.75);
+        this.scene.rotate(135*Math.PI/180,0,0,1);
+        this.triangle.display();
+        this.scene.popMatrix();
+	}
+}
+
